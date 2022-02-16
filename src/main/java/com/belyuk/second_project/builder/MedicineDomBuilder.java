e1244b22414e454b9cff1d06feab0155c0aa6941
MihailBelyuk <Tamerlan123Sadist123>
2022-02-16 10:25:30 +0300

    XML

--- src/main/java/com/belyuk/second_project/builder/MedicineDomBuilder.java
+++ src/main/java/com/belyuk/second_project/builder/MedicineDomBuilder.java
@@ -23,6 +23,21 @@ public class MedicineDomBuilder {
 
   private Set<Medicine> medicineSet;
   private DocumentBuilder documentBuilder;
+  private static final String NAME = "name";
+  private static final String MANUFACTURER = "manufacturer";
+  private static final String DOSAGE_FORM = "dosage_form";
+  private static final String AMOUNT = "amount";
+  private static final String ID = "id";
+  private static final String DOSAGE = "dosage";
+  private static final String PRICE = "price";
+  private static final String ISSUE_DATE = "issue_date";
+  private static final String EXPIRY_DATE = "expiry_date";
+  private static final String SOLUBILITY = "solubility";
+  private static final String SERIAL = "serial";
+  private static final String DATE_PATTERN = "dd.MM.yyyy";
+
+
+
 
   public MedicineDomBuilder() {
     medicineSet = new HashSet<Medicine>();
@@ -59,33 +74,33 @@ public class MedicineDomBuilder {
 
   private Antibiotics buildAntibiotics(Element antibioticsElement) {
     Antibiotics antibiotics = new Antibiotics();
-    antibiotics.setName(getElementTextContent(antibioticsElement, "name"));
-    antibiotics.setManufacturer(getElementTextContent(antibioticsElement, "manufacturer"));
-    antibiotics.setDosageForm(getElementTextContent(antibioticsElement, "dosage_form"));
-    antibiotics.setAmount(Integer.parseInt(getElementTextContent(antibioticsElement, "amount")));
-    antibiotics.setId(antibioticsElement.getAttribute("id"));
-    antibiotics.setDosage(getElementTextContent(antibioticsElement, "dosage"));
-    antibiotics.setPrice(getElementBigDecimalContent(antibioticsElement, "price"));
-    antibiotics.setMedCertificateIssueDate(getElementDateContent(antibioticsElement, "issue_date"));
+    antibiotics.setName(getElementTextContent(antibioticsElement, NAME));
+    antibiotics.setManufacturer(getElementTextContent(antibioticsElement, MANUFACTURER));
+    antibiotics.setDosageForm(getElementTextContent(antibioticsElement, DOSAGE_FORM));
+    antibiotics.setAmount(Integer.parseInt(getElementTextContent(antibioticsElement, AMOUNT)));
+    antibiotics.setId(antibioticsElement.getAttribute(ID));
+    antibiotics.setDosage(getElementTextContent(antibioticsElement, DOSAGE));
+    antibiotics.setPrice(getElementBigDecimalContent(antibioticsElement, PRICE));
+    antibiotics.setMedCertificateIssueDate(getElementDateContent(antibioticsElement, ISSUE_DATE));
     antibiotics.setMedCertificateExpiryDate(
-        getElementDateContent(antibioticsElement, "expiry_date"));
+        getElementDateContent(antibioticsElement, EXPIRY_DATE));
 
     return antibiotics;
   }
 
   private Vitamins buildVitamins(Element vitaminsElement) {
     Vitamins vitamins = new Vitamins();
-    vitamins.setName(getElementTextContent(vitaminsElement, "name"));
-    vitamins.setManufacturer(getElementTextContent(vitaminsElement, "manufacturer"));
-    vitamins.setDosageForm(getElementTextContent(vitaminsElement, "dosage_form"));
-    vitamins.setAmount(Integer.parseInt(getElementTextContent(vitaminsElement, "amount")));
-    vitamins.setId(vitaminsElement.getAttribute("id"));
-    vitamins.setDosage(getElementTextContent(vitaminsElement, "dosage"));
-    vitamins.setPrice(getElementBigDecimalContent(vitaminsElement, "price"));
-    vitamins.setMedCertificateIssueDate(getElementDateContent(vitaminsElement, "issue_date"));
-    vitamins.setMedCertificateExpiryDate(getElementDateContent(vitaminsElement, "expiry_date"));
-    vitamins.setSolubility(getElementTextContent(vitaminsElement, "solubility"));
-    vitamins.setSerial(vitaminsElement.getAttribute("serial"));
+    vitamins.setName(getElementTextContent(vitaminsElement, NAME));
+    vitamins.setManufacturer(getElementTextContent(vitaminsElement, MANUFACTURER));
+    vitamins.setDosageForm(getElementTextContent(vitaminsElement, DOSAGE_FORM));
+    vitamins.setAmount(Integer.parseInt(getElementTextContent(vitaminsElement, AMOUNT)));
+    vitamins.setId(vitaminsElement.getAttribute(ID));
+    vitamins.setDosage(getElementTextContent(vitaminsElement, DOSAGE));
+    vitamins.setPrice(getElementBigDecimalContent(vitaminsElement, PRICE));
+    vitamins.setMedCertificateIssueDate(getElementDateContent(vitaminsElement, ISSUE_DATE);
+    vitamins.setMedCertificateExpiryDate(getElementDateContent(vitaminsElement, EXPIRY_DATE));
+    vitamins.setSolubility(getElementTextContent(vitaminsElement, SOLUBILITY));
+    vitamins.setSerial(vitaminsElement.getAttribute(SERIAL));
     return vitamins;
   }
 
@@ -99,7 +114,7 @@ public class MedicineDomBuilder {
   private static Date getElementDateContent(Element element, String elementName) {
     NodeList nList = element.getElementsByTagName(elementName);
     Node node = nList.item(0);
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_PATTERN);
     Date date = null;
     try {
       date = simpleDateFormat.parse(node.getTextContent());
